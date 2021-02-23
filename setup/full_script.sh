8fbd56ca170c3d998fce948b94db1a590a050efb
Ameya Deshpande <ameyanrd@outlook.com>
2021-02-24 00:41:28 +0530

    Completed Setup upto Controller nodes bootstrapping

--- setup/full_script.sh
+++ setup/full_script.sh
@@ -39,11 +39,15 @@ kubectl version --client
 # Provisioning Compute Resources
 echo -e "${RED}Provisioning Compute Resources${NC}\n"
 ## Create a dedicated VPC
+echo -e "${YEL}Create a dedicated VPC: kubernetes-the-hard-way${NC}\n"
 gcloud compute networks create kubernetes-the-hard-way --subnet-mode custom
 ## Create a subnet for VMs in this VPC
+echo -e "${YEL}Create a subnet for VMs in this VPC${NC}\n"
 gcloud compute networks subnets create kubernetes \
   --network kubernetes-the-hard-way \
   --range 10.240.0.0/24
+
+echo -e "${YEL}Create the Internal and External communication firewalls${NC}\n"
 ## Create a firewall for internal communication across all protocols
 gcloud compute firewall-rules create kubernetes-the-hard-way-allow-internal \
   --allow tcp,udp,icmp \
@@ -64,6 +68,7 @@ gcloud compute addresses create kubernetes-the-hard-way \
 echo -e "${YEL}Verify the kubernetes-the-hard-way static IP address was created in your default compute region${NC}\n"
 gcloud compute addresses list --filter="name=('kubernetes-the-hard-way')"
 ## Create two compute instances which will host the Kubernetes control plane
+echo -e "${YEL}Create controller instances${NC}\n"
 for i in 0 1; do
   gcloud compute instances create controller-${i} \
     --async \
@@ -78,6 +83,7 @@ for i in 0 1; do
     --tags kubernetes-the-hard-way,controller
 done
 ## Create two compute instances which will host the Kubernetes worker nodes
+echo -e "${YEL}Create worker instances${NC}\n"
 for i in 0 1; do
   gcloud compute instances create worker-${i} \
     --async \
@@ -533,7 +539,7 @@ resources:
 EOF
 ## Copy the encryption-config.yaml encryption config file to each controller instance
 echo -e "${YEL}Copy the encryption-config.yaml encryption config file to each controller instance${NC}"
-for instance in controller-0 controller-1 controller-2; do
+for instance in controller-0 controller-1; do
   gcloud compute scp encryption-config.yaml ${instance}:~/
 done
 
@@ -541,7 +547,403 @@ done
 echo -e "${RED}Bootstrapping the etcd cluster${NC}"
 
 ## SSH into controllers
-#echo -e "${YEL}SSH into controllers${NC}"
-#for instance in controller-0 controller-1; do
-#    gcloud compute ssh ${instance}
-#done
+echo -e "${YEL}SSH into controllers${NC}"
+for instance in controller-0 controller-1; do
+    cat > bs_etcd.sh << EOF1
+# Colors
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "\${YEL}Download the official etcd release binaries from the etcd GitHub project\${NC}\n"
+wget -q --show-progress --https-only --timestamping \
+  "https://github.com/etcd-io/etcd/releases/download/v3.4.10/etcd-v3.4.10-linux-amd64.tar.gz"
+
+echo -e "\${YEL}Extract and install the etcd server and the etcdctl command line utility\${NC}\n"
+{
+  tar -xvf etcd-v3.4.10-linux-amd64.tar.gz
+  sudo mv etcd-v3.4.10-linux-amd64/etcd* /usr/local/bin/
+}
+
+echo -e "\${YEL}Configure the etcd Server\${NC}\n"
+{
+  sudo mkdir -p /etc/etcd /var/lib/etcd
+  sudo chmod 700 /var/lib/etcd
+  sudo cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/
+}
+
+echo -e "\${YEL}Retrieve the internal IP address for the current compute instance\${NC}\n"
+INTERNAL_IP=\$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+
+echo -e "\${YEL}Set the etcd name to match the hostname of the current compute instance\${NC}\n"
+ETCD_NAME=\$(hostname -s)
+
+echo -e "\${YEL}Create the etcd.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/etcd.service
+[Unit]
+Description=etcd
+Documentation=https://github.com/coreos
+
+[Service]
+Type=notify
+ExecStart=/usr/local/bin/etcd \\
+  --name \${ETCD_NAME} \\
+  --cert-file=/etc/etcd/kubernetes.pem \\
+  --key-file=/etc/etcd/kubernetes-key.pem \\
+  --peer-cert-file=/etc/etcd/kubernetes.pem \\
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \\
+  --trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \\
+  --peer-client-cert-auth \\
+  --client-cert-auth \\
+  --initial-advertise-peer-urls https://\${INTERNAL_IP}:2380 \\
+  --listen-peer-urls https://\${INTERNAL_IP}:2380 \\
+  --listen-client-urls https://\${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://\${INTERNAL_IP}:2379 \\
+  --initial-cluster-token etcd-cluster-0 \\
+  --initial-cluster controller-0=https://10.240.0.10:2380,controller-1=https://10.240.0.11:2380,controller-2=https://10.240.0.12:2380 \\
+  --initial-cluster-state new \\
+  --data-dir=/var/lib/etcd
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\${YEL}Start the etcd Server\${NC}\n"
+{
+  sudo systemctl daemon-reload
+  sudo systemctl enable etcd
+  sudo systemctl start etcd
+}
+
+## Verification of etcd cluster members
+echo -e "\${YEL}Verification: List of the etcd cluster members\${NC}\n"
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem
+
+EOF1
+    chmod +x bs_etcd.sh
+    gcloud compute scp bs_etcd.sh ${instance}:.
+    rm -rf etcd.sh
+    echo -e "${YEL}SSHing into ${instance}...\n\n Please run bs_etcd.sh inside it and exit.${NC}\n"
+    gcloud compute ssh ${instance}
+done
+
+# Bootstrapping the Kubernetes Control Plane
+echo -e "${RED}Bootstrapping the Kubernetes Control Plane${NC}\n"
+
+## SSH into controllers
+echo -e "${YEL}SSH into controllers${NC}\n"
+for instance in controller-0 controller-1; do
+    cat > bs_kcp.sh << EOF1
+# Colors
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "\${YEL}Create the Kubernetes configuration directory\${NC}\n"
+sudo mkdir -p /etc/kubernetes/config
+
+echo -e "\${YEL}Download the official Kubernetes release binaries\${NC}\n"
+wget -q --show-progress --https-only --timestamping \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kube-apiserver" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kube-controller-manager" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kube-scheduler" \
+  "https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kubectl"
+
+echo -e "\${YEL}Install Kubernetes binaries\${NC}\n"
+{
+  chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
+  sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
+}
+
+echo -e "\${YEL}Configure the Kubernetes API Server\${NC}\n"
+{
+  echo -e "\${YEL}1. Make /var/lib/kubernetes/\${NC}\n"
+  sudo mkdir -p /var/lib/kubernetes/
+
+  echo -e "\${YEL}2. Move the CA, Kubernetes (apiserver) and service-account certificates and private keys to the above folder\${NC}\n"
+  sudo mv ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
+    service-account-key.pem service-account.pem \
+    encryption-config.yaml /var/lib/kubernetes/
+}
+
+## Fetch internal IP used to advertise the API Server
+echo -e "\${YEL}3. Fetch internal IP used to advertise the API Server\${NC}\n"
+INTERNAL_IP=\$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+
+## Create the kube-apiserver.service systemd unit file
+echo -e "\${YEL}4. Create the kube-apiserver.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/kube-apiserver.service
+[Unit]
+Description=Kubernetes API Server
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-apiserver \\
+  --advertise-address=\${INTERNAL_IP} \\
+  --allow-privileged=true \\
+  --apiserver-count=3 \\
+  --audit-log-maxage=30 \\
+  --audit-log-maxbackup=3 \\
+  --audit-log-maxsize=100 \\
+  --audit-log-path=/var/log/audit.log \\
+  --authorization-mode=Node,RBAC \\
+  --bind-address=0.0.0.0 \\
+  --client-ca-file=/var/lib/kubernetes/ca.pem \\
+  --enable-admission-plugins=NamespaceLifecycle,NodeRestriction,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota \\
+  --etcd-cafile=/var/lib/kubernetes/ca.pem \\
+  --etcd-certfile=/var/lib/kubernetes/kubernetes.pem \\
+  --etcd-keyfile=/var/lib/kubernetes/kubernetes-key.pem \\
+  --etcd-servers=https://10.240.0.10:2379,https://10.240.0.11:2379,https://10.240.0.12:2379 \\
+  --event-ttl=1h \\
+  --encryption-provider-config=/var/lib/kubernetes/encryption-config.yaml \\
+  --kubelet-certificate-authority=/var/lib/kubernetes/ca.pem \\
+  --kubelet-client-certificate=/var/lib/kubernetes/kubernetes.pem \\
+  --kubelet-client-key=/var/lib/kubernetes/kubernetes-key.pem \\
+  --kubelet-https=true \\
+  --runtime-config='api/all=true' \\
+  --service-account-key-file=/var/lib/kubernetes/service-account.pem \\
+  --service-cluster-ip-range=10.32.0.0/24 \\
+  --service-node-port-range=30000-32767 \\
+  --tls-cert-file=/var/lib/kubernetes/kubernetes.pem \\
+  --tls-private-key-file=/var/lib/kubernetes/kubernetes-key.pem \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## Configure the Kubernetes Controller Manager
+echo -e "\${YEL}Configure the Kubernetes Controller Manager\${NC}\n"
+echo -e "\${YEL}1. Move the kube-controller-manager kubeconfig into /var/lib/kubernetes\${NC}\n"
+sudo mv kube-controller-manager.kubeconfig /var/lib/kubernetes/
+
+echo -e "\${YEL}2. Create the kube-controller-manager.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/kube-controller-manager.service
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-controller-manager \\
+  --bind-address=0.0.0.0 \\
+  --cluster-cidr=10.200.0.0/16 \\
+  --cluster-name=kubernetes \\
+  --cluster-signing-cert-file=/var/lib/kubernetes/ca.pem \\
+  --cluster-signing-key-file=/var/lib/kubernetes/ca-key.pem \\
+  --kubeconfig=/var/lib/kubernetes/kube-controller-manager.kubeconfig \\
+  --leader-elect=true \\
+  --root-ca-file=/var/lib/kubernetes/ca.pem \\
+  --service-account-private-key-file=/var/lib/kubernetes/service-account-key.pem \\
+  --service-cluster-ip-range=10.32.0.0/24 \\
+  --use-service-account-credentials=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## Configure the Kubernetes Scheduler
+echo -e "\${YEL}Configure the Kubernetes Scheduler\${NC}\n"
+echo -e "\${YEL}1. Move the kube-scheduler kubeconfig into /var/lib/kubernetes\${NC}\n"
+sudo mv kube-scheduler.kubeconfig /var/lib/kubernetes/
+
+echo -e "\${YEl}2. Create the kube-scheduler.yaml configuration file\${NC}\n"
+cat <<EOF | sudo tee /etc/kubernetes/config/kube-scheduler.yaml
+apiVersion: kubescheduler.config.k8s.io/v1alpha1
+kind: KubeSchedulerConfiguration
+clientConnection:
+  kubeconfig: "/var/lib/kubernetes/kube-scheduler.kubeconfig"
+leaderElection:
+  leaderElect: true
+EOF
+
+echo -e "\${YEL}3. Create the kube-scheduler.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/kube-scheduler.service
+[Unit]
+Description=Kubernetes Scheduler
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-scheduler \\
+  --config=/etc/kubernetes/config/kube-scheduler.yaml \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+## Start the Controller Services (API server, Control Manager, Kube-scheduler)
+echo -e "\${YEL}Start the Controller Services (API server, Control Manager, Kube-scheduler)\${NC}\n"
+{
+  sudo systemctl daemon-reload
+  sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
+  sudo systemctl start kube-apiserver kube-controller-manager kube-scheduler
+}
+
+## Enable HTTP Health Checks
+echo -e "\${YEL}Enable HTTP Health Checks\${NC}\n"
+
+echo -e "\${YEL}1. Install a basic web server to handle HTTP health checks\${NC}\n"
+sudo apt-get update
+sudo apt-get install -y nginx
+
+echo -e "\${YEL}2. Configure the Nginx server for health check endpoint\${NC}\n"
+cat > kubernetes.default.svc.cluster.local <<EOF
+server {
+  listen      80;
+  server_name kubernetes.default.svc.cluster.local;
+
+  location /healthz {
+     proxy_pass                    https://127.0.0.1:6443/healthz;
+     proxy_ssl_trusted_certificate /var/lib/kubernetes/ca.pem;
+  }
+}
+EOF
+
+echo -e "\${YEL}3. Move the above file to sites-available and create a soft link to sites-enabled\${NC}\n"
+{
+  sudo mv kubernetes.default.svc.cluster.local \
+    /etc/nginx/sites-available/kubernetes.default.svc.cluster.local
+
+  sudo ln -s /etc/nginx/sites-available/kubernetes.default.svc.cluster.local /etc/nginx/sites-enabled/
+}
+
+echo -e "\${YEL}4. Restart and Enable Nginx\${NC}\n"
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+echo -e "\${YEL}Verification of Controller Services and HTTP health-check proxy\${NC}\n"
+echo -e "\${YEL}1. Check the components (use admin kubeconfig for authentication)\${NC}\n"
+kubectl get componentstatuses --kubeconfig admin.kubeconfig
+
+echo -e "\${YEL}2. Test the nginx HTTP health check proxy\${NC}\n"
+curl -H "Host: kubernetes.default.svc.cluster.local" -i http://127.0.0.1/healthz
+
+EOF1
+    chmod +x bs_kcp.sh
+    gcloud compute scp bs_kcp.sh ${instance}:.
+    rm -rf bs_kcp.sh
+    echo -e "${YEL}SSHing into ${instance}...\n\n Please run bs_kcp.sh inside it and exit.${NC}\n"
+    gcloud compute ssh ${instance}
+done
+
+## RBAC for Kubelet Authorization
+echo -e "${YEL}RBAC for Kubelet Authorization${NC}\n"
+echo -e "${YEL}(Access to the Kubelet API is required for retrieving metrics, logs, and executing commands in pods.)${NC}\n"
+echo -e "${YEL}(Commands need to be executed in any one of the controllers)${NC}\n"
+
+cat > bs_rbac.sh << EOF1
+
+# Colors
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
+## Create the system:kube-apiserver-to-kubelet ClusterRole with permissions to access the Kubelet API and perform most common tasks associated with managing pods
+echo -e "\${YEL}Create the system:kube-apiserver-to-kubelet ClusterRole with permissions to access the Kubelet API and perform most common tasks associated with managing pods\${NC}\n"
+
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:kube-apiserver-to-kubelet
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes/proxy
+      - nodes/stats
+      - nodes/log
+      - nodes/spec
+      - nodes/metrics
+    verbs:
+      - "*"
+EOF
+
+## Bind the system:kube-apiserver-to-kubelet ClusterRole to the kubernetes user
+echo -e "\${YEL}Bind the system:kube-apiserver-to-kubelet ClusterRole to the kubernetes user\${NC}\n"
+
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
+
+EOF1
+
+chmod +x bs_rbac.sh
+gcloud compute scp bs_rbac.sh controller-0:.
+rm -rf bs_rbac.sh
+echo -e "${YEL}SSHing into Controller-0...\n\n Please run bs_rbac.sh inside it and exit.${NC}\n"
+gcloud compute ssh controller-0
+
+## The Kubernetes Frontend Load Balancer
+echo -e "${YEL}The Kubernetes Frontend Load Balancer${NC}\n"
+
+{
+  echo -e "${YEL}1. Get the public IP address for Load Balancer${NC}\n"
+  KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+    --region $(gcloud config get-value compute/region) \
+    --format 'value(address)')
+  echo -e "${YEL}2. Create HTTP health checks on path /healthz${NC}\n"
+  gcloud compute http-health-checks create kubernetes \
+    --description "Kubernetes Health Check" \
+    --host "kubernetes.default.svc.cluster.local" \
+    --request-path "/healthz"
+  echo -e "${YEL}3. Create Firewall Rule to allow for the health check${NC}\n"
+  gcloud compute firewall-rules create kubernetes-the-hard-way-allow-health-check \
+    --network kubernetes-the-hard-way \
+    --source-ranges 209.85.152.0/22,209.85.204.0/22,35.191.0.0/16 \
+    --allow tcp
+  echo -e "${YEL}4. Create a target pool for the controllers where the above HTTP health check takes place${NC}\n"
+  gcloud compute target-pools create kubernetes-target-pool \
+    --http-health-check kubernetes
+  echo -e "${YEL}Add the instances controller-n to the target pool${NC}\n"
+  gcloud compute target-pools add-instances kubernetes-target-pool \
+   --instances controller-0,controller-1
+  echo -e "${YEL}Create a forwarding rule on the port of KUBERNETES_PUBLIC_ADDRESS${NC}\n"
+  gcloud compute forwarding-rules create kubernetes-forwarding-rule \
+    --address ${KUBERNETES_PUBLIC_ADDRESS} \
+    --ports 6443 \
+    --region $(gcloud config get-value compute/region) \
+    --target-pool kubernetes-target-pool
+}
+
+## Verification
+echo -e "${YEL}Verification${NC}\n"
+echo -e "${YEL}1. Retrieve the kubernetes-the-hard-way static IP address${NC}\n"
+KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+  --region $(gcloud config get-value compute/region) \
+  --format 'value(address)')
+
+echo -e "${YEL}2. Make a HTTPS request for the Kubernetes version INFO:${NC}\n"
+curl --cacert ca.pem https://${KUBERNETES_PUBLIC_ADDRESS}:6443/version
