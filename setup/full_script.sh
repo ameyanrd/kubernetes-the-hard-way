32b38c0e5b45cb180fa5b986416927f17398632c
Ameya Deshpande <ameyanrd@outlook.com>
2021-03-26 17:14:56 +0530

    Full Script up to DNS-core

--- setup/full_script.sh
+++ setup/full_script.sh
@@ -947,3 +947,306 @@ KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-har
 
 echo -e "${YEL}2. Make a HTTPS request for the Kubernetes version INFO:${NC}\n"
 curl --cacert ca.pem https://${KUBERNETES_PUBLIC_ADDRESS}:6443/version
+
+# Bootstrapping the Kubernetes Worker Nodes
+
+echo -e "${RED}Bootstrapping the Kubernetes Worker Nodes${NC}\n"
+
+for instance in worker-0 worker-1; do
+    cat > bs_kwn.sh << EOF1
+# Colors
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "\${YEL}Install the OS dependencies\${NC}\n"
+{
+  sudo apt-get update
+  sudo apt-get -y install socat conntrack ipset
+}
+
+echo -e "\${YEL}Diable Swap\${NC}\n"
+sudo swapoff -a
+
+echo -e "\${YEL}Download and Install Worker Binaries\${NC}\n"
+wget -q --show-progress --https-only --timestamping \
+  https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.18.0/crictl-v1.18.0-linux-amd64.tar.gz \
+  https://github.com/opencontainers/runc/releases/download/v1.0.0-rc91/runc.amd64 \
+  https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v0.8.6.tgz \
+  https://github.com/containerd/containerd/releases/download/v1.3.6/containerd-1.3.6-linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kubelet
+
+echo -e "\${YEL}Create the installation directories\${NC}\n"
+sudo mkdir -p \
+  /etc/cni/net.d \
+  /opt/cni/bin \
+  /var/lib/kubelet \
+  /var/lib/kube-proxy \
+  /var/lib/kubernetes \
+  /var/run/kubernetes
+
+echo -e "\${YEL}Install the worker binaries\${NC}\n"
+{
+  mkdir containerd
+  tar -xvf crictl-v1.18.0-linux-amd64.tar.gz
+  tar -xvf containerd-1.3.6-linux-amd64.tar.gz -C containerd
+  sudo tar -xvf cni-plugins-linux-amd64-v0.8.6.tgz -C /opt/cni/bin/
+  sudo mv runc.amd64 runc
+  chmod +x crictl kubectl kube-proxy kubelet runc
+  sudo mv crictl kubectl kube-proxy kubelet runc /usr/local/bin/
+  sudo mv containerd/bin/* /bin/
+}
+
+echo -e "\${YEL}Configure CNI Networking\${NC}\n"
+
+echo -e "\${YEL}1. Retrieve the Pod CIDR range for the current compute instance\${NC}\n"
+POD_CIDR=\$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/attributes/pod-cidr)
+
+echo -e "\${YEL}2. Create the bridge network configuration file\${NC}\n"
+cat <<EOF | sudo tee /etc/cni/net.d/10-bridge.conf
+{
+    "cniVersion": "0.3.1",
+    "name": "bridge",
+    "type": "bridge",
+    "bridge": "cnio0",
+    "isGateway": true,
+    "ipMasq": true,
+    "ipam": {
+        "type": "host-local",
+        "ranges": [
+          [{"subnet": "\${POD_CIDR}"}]
+        ],
+        "routes": [{"dst": "0.0.0.0/0"}]
+    }
+}
+EOF
+
+echo -e "\${YEL}3. Create the loopback network configuration file\${NC}\n"
+cat <<EOF | sudo tee /etc/cni/net.d/99-loopback.conf
+{
+    "cniVersion": "0.3.1",
+    "name": "lo",
+    "type": "loopback"
+}
+EOF
+
+echo -e "\${YEL}Configure containerd\${NC}\n"
+
+echo -e "\${YEL}1. Create the containerd configuration file\${NC}\n"
+sudo mkdir -p /etc/containerd/
+
+cat << EOF | sudo tee /etc/containerd/config.toml
+[plugins]
+  [plugins.cri.containerd]
+    snapshotter = "overlayfs"
+    [plugins.cri.containerd.default_runtime]
+      runtime_type = "io.containerd.runtime.v1.linux"
+      runtime_engine = "/usr/local/bin/runc"
+      runtime_root = ""
+EOF
+
+echo -e "\${YEL}2. Create the containerd.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/containerd.service
+[Unit]
+Description=containerd container runtime
+Documentation=https://containerd.io
+After=network.target
+
+[Service]
+ExecStartPre=/sbin/modprobe overlay
+ExecStart=/bin/containerd
+Restart=always
+RestartSec=5
+Delegate=yes
+KillMode=process
+OOMScoreAdjust=-999
+LimitNOFILE=1048576
+LimitNPROC=infinity
+LimitCORE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\${YEL}Configure the Kubelet\${NC}\n"
+{
+  sudo mv \${HOSTNAME}-key.pem \${HOSTNAME}.pem /var/lib/kubelet/
+  sudo mv \${HOSTNAME}.kubeconfig /var/lib/kubelet/kubeconfig
+  sudo mv ca.pem /var/lib/kubernetes/
+}
+
+echo -e "\${YEL}1. Create the kubelet-config.yaml configuration file\${NC}\n"
+cat <<EOF | sudo tee /var/lib/kubelet/kubelet-config.yaml
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+authentication:
+  anonymous:
+    enabled: false
+  webhook:
+    enabled: true
+  x509:
+    clientCAFile: "/var/lib/kubernetes/ca.pem"
+authorization:
+  mode: Webhook
+clusterDomain: "cluster.local"
+clusterDNS:
+  - "10.32.0.10"
+podCIDR: "\${POD_CIDR}"
+resolvConf: "/run/systemd/resolve/resolv.conf"
+runtimeRequestTimeout: "15m"
+tlsCertFile: "/var/lib/kubelet/\${HOSTNAME}.pem"
+tlsPrivateKeyFile: "/var/lib/kubelet/\${HOSTNAME}-key.pem"
+EOF
+
+echo -e "\${YEL}Create the kubelet.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service
+[Unit]
+Description=Kubernetes Kubelet
+Documentation=https://github.com/kubernetes/kubernetes
+After=containerd.service
+Requires=containerd.service
+
+[Service]
+ExecStart=/usr/local/bin/kubelet \\
+  --config=/var/lib/kubelet/kubelet-config.yaml \\
+  --container-runtime=remote \\
+  --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \\
+  --image-pull-progress-deadline=2m \\
+  --kubeconfig=/var/lib/kubelet/kubeconfig \\
+  --network-plugin=cni \\
+  --register-node=true \\
+  --v=2
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\${YEL}Configure the Kubernetes Proxy\${NC}\n"
+sudo mv kube-proxy.kubeconfig /var/lib/kube-proxy/kubeconfig
+
+echo -e "\${YEL}1. Create the kube-proxy-config.yaml configuration file\${NC}\n"
+cat <<EOF | sudo tee /var/lib/kube-proxy/kube-proxy-config.yaml
+kind: KubeProxyConfiguration
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+clientConnection:
+  kubeconfig: "/var/lib/kube-proxy/kubeconfig"
+mode: "iptables"
+clusterCIDR: "10.200.0.0/16"
+EOF
+
+echo -e "\${YEL}2. Create the kube-proxy.service systemd unit file\${NC}\n"
+cat <<EOF | sudo tee /etc/systemd/system/kube-proxy.service
+[Unit]
+Description=Kubernetes Kube Proxy
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+ExecStart=/usr/local/bin/kube-proxy \\
+  --config=/var/lib/kube-proxy/kube-proxy-config.yaml
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\${YEL}Start the Worker Services\${NC}\n"
+{
+  sudo systemctl daemon-reload
+  sudo systemctl enable containerd kubelet kube-proxy
+  sudo systemctl start containerd kubelet kube-proxy
+}
+EOF1
+    chmod +x bs_kwn.sh
+    gcloud compute scp bs_kwn.sh ${instance}:.
+    rm -rf bs_kwn.sh
+    echo -e "${YEL}SSHing into ${instance}...\n\n Please run bs_kwn.sh inside it and exit.${NC}\n"
+    gcloud compute ssh ${instance}
+done
+
+gcloud compute ssh controller-0 \
+  --command "kubectl get nodes --kubeconfig admin.kubeconfig"
+
+# Configuring kubectl for Remote Access
+echo -e "${RED}Configuring kubectl for Remote Access${NC}\n"
+
+echo -e "${YEL}Generate a kubeconfig file suitable for authenticating as the admin user${NC}\n"
+{
+  KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+    --region $(gcloud config get-value compute/region) \
+    --format 'value(address)')
+
+  kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
+
+  kubectl config set-credentials admin \
+    --client-certificate=admin.pem \
+    --client-key=admin-key.pem
+
+  kubectl config set-context kubernetes-the-hard-way \
+    --cluster=kubernetes-the-hard-way \
+    --user=admin
+
+  kubectl config use-context kubernetes-the-hard-way
+}
+
+## Verification
+echo -e "${YEL}Verification${NC}\n"
+echo -e "${YEL}1. Check the health of the remote Kubernetes cluster${NC}\n"
+kubectl get componentstatuses
+
+echo -e "${YEL}2. List the nodes in the remote Kubernetes cluster${NC}\n"
+kubectl get nodes
+
+# Provisioning Pod Network Routes
+
+echo -e "${RED}Provisioning Pod Network Routes${NC}\n"
+
+echo -e "${RED}Print the internal IP address and Pod CIDR range for each worker instance${NC}\n"
+for instance in worker-0 worker-1; do
+  gcloud compute instances describe ${instance} \
+    --format 'value[separator=" "](networkInterfaces[0].networkIP,metadata.items[0].value)'
+done
+
+echo -e "${YEL}Routes${NC}\n"
+echo -e "${YEL}1. Create network routes for each worker instance${NC}\n"
+for i in 0 1 2; do
+  gcloud compute routes create kubernetes-route-10-200-${i}-0-24 \
+    --network kubernetes-the-hard-way \
+    --next-hop-address 10.240.0.2${i} \
+    --destination-range 10.200.${i}.0/24
+done
+
+echo -e "${YEL}List the routes in the kubernetes-the-hard-way VPC network${NC}\n"
+gcloud compute routes list --filter "network: kubernetes-the-hard-way"
+
+# Deploying the DNS Cluster Add-on
+echo -e "${RED}Deploying the DNS Cluster Add-on${NC}\n"
+
+echo -e "${YEL}The DNS Cluster Add-on${NC}\n"
+echo -e "${YEL}1. Deploy the coredns cluster add-on${NC}\n"
+kubectl apply -f https://storage.googleapis.com/kubernetes-the-hard-way/coredns-1.7.0.yaml
+
+echo -e "${YEL}2. List the pods created by the kube-dns deployment${NC}\n"
+kubectl get pods -l k8s-app=kube-dns -n kube-system
+
+echo -e "${YEL}Verification${NC}\n"
+echo -e "${YEL}1. Create a busybox deployment${NC}\n"
+kubectl run busybox --image=busybox:1.28 --command -- sleep 3600
+
+echo -e "${YEL}2. List the pod created by the busybox deployment${NC}\n"
+kubectl get pods -l run=busybox
+
+echo -e "${YEL}3. Retrieve the full name of the busybox pod${NC}\n"
+POD_NAME=$(kubectl get pods -l run=busybox -o jsonpath="{.items[0].metadata.name}")
+
+echo -e "${YEL}4. Execute a DNS lookup for the kubernetes service inside the busybox pod${NC}\n"
+kubectl exec -ti $POD_NAME -- nslookup kubernetes
+
